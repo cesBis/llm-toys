96dd35e4359db5b9b19eb39dab207764e4acfa38
Conner Smith <smith_ces@outlook.com>
2024-08-26 07:39:35 -0400

    try out ollama

--- README.md
+++ README.md
@@ -7,6 +7,4 @@ Using Python 3.10.12 and `python -m venv pyenv; pip install -r requirements.txt`
 ## to-do
 
 - [ ] play with temperature paramater
-- [ ] swap out azure api calls with a local llama instance
-    - https://ai.meta.com/blog/5-steps-to-getting-started-with-llama-2/
-    - https://github.com/meta-llama/llama/blob/main/example_chat_completion.py
+- [x] swap out azure api calls with a local llama instance
---
+++ ollama-trancendentalism.py
@@ -0,0 +1,20 @@
+from ollama import Client
+
+client = Client(
+    host='http://llama:11434',
+    timeout=8000,
+)
+
+text_output = open('trancendentalism.md', 'a')
+for number in range(1, 13, 1):
+    prompt = f'Explain the transcendentalist school of thought at a grade {number} reading level'
+    text_output.write(f'\n\n## {prompt}\n\n')
+    response = client.chat(
+        model = "llama3.1",
+        # https://platform.openai.com/docs/guides/chat-completions/message-roles
+        messages = [
+            {"role" : "user", "content" : prompt}
+        ],
+    )
+    text_output.write(response['message']['content'])
+text_output.close()
