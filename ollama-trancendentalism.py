efc69684f7d4d5184bf35089671f8f4ed1e342e2
Conner Smith <smith_ces@outlook.com>
2024-09-16 20:02:00 -0400

    trancendentalism by hermes3, gemma2, and phi3.5

--- ollama-trancendentalism.py
+++ ollama-trancendentalism.py
@@ -5,16 +5,21 @@ client = Client(
     timeout=8000,
 )
 
-text_output = open('trancendentalism.md', 'a')
-for number in range(1, 13, 1):
-    prompt = f'Explain the transcendentalist school of thought at a grade {number} reading level'
-    text_output.write(f'\n\n## {prompt}\n\n')
-    response = client.chat(
-        model = "smollm:135m",
-        # https://platform.openai.com/docs/guides/chat-completions/message-roles
-        messages = [
-            {"role" : "user", "content" : prompt}
-        ],
-    )
-    text_output.write(response['message']['content'])
-text_output.close()
+for model in [
+    'hermes3',
+    'gemma2',
+    'phi3.5',
+    ]:
+    text_output = open(model+'-trancendentalism.md', 'a')
+    for number in range(1, 13, 1):
+        prompt = f'Explain the transcendentalist school of thought at a grade {number} reading level'
+        text_output.write(f'\n\n## {prompt}\n\n')
+        response = client.chat(
+            model = model,
+            # https://platform.openai.com/docs/guides/chat-completions/message-roles
+            messages = [
+                {"role" : "user", "content" : prompt}
+            ],
+        )
+        text_output.write(response['message']['content'])
+    text_output.close()
