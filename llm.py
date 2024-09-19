13163df5a0ed93ec9ace589d7f6862641f70f673
Conner Smith <smith_ces@outlook.com>
2024-09-18 21:12:43 -0400

    introduce ruff

--- llm.py
+++ llm.py
@@ -1,22 +1,23 @@
 import ollama
 import json
 
-def write_response(prompt,
-                   system_prompt,
-                   model,
-                   options = {},
-                   json_name = 'response',
-                   client = ollama.Client(
-                                host='http://llama.moo:11434',
-                                timeout=8000,
-                            )
-                   ):
 
+def write_response(
+    prompt,
+    system_prompt,
+    model,
+    options={},
+    json_name="response",
+    client=ollama.Client(
+        host="http://llama.moo:11434",
+        timeout=8000,
+    ),
+):
     response = client.generate(
-        prompt = prompt,
-        system = system_prompt,
-        model = model,
-        options = options,
+        prompt=prompt,
+        system=system_prompt,
+        model=model,
+        options=options,
     )
 
-    json.dump(response, open(json_name + '.json', 'w'))
+    json.dump(response, open(json_name + ".json", "w"))
--- ollama-trancendentalism.py
+++ ollama-trancendentalism.py
@@ -1,25 +1,23 @@
 from ollama import Client
 
 client = Client(
-    host='http://llama:11434',
+    host="http://llama:11434",
     timeout=8000,
 )
 
 for model in [
-    'hermes3',
-    'gemma2',
-    'phi3.5',
-    ]:
-    text_output = open(model+'-trancendentalism.md', 'a')
+    "hermes3",
+    "gemma2",
+    "phi3.5",
+]:
+    text_output = open(model + "-trancendentalism.md", "a")
     for number in range(1, 13, 1):
-        prompt = f'Explain the transcendentalist school of thought at a grade {number} reading level'
-        text_output.write(f'\n\n## {prompt}\n\n')
+        prompt = f"Explain the transcendentalist school of thought at a grade {number} reading level"
+        text_output.write(f"\n\n## {prompt}\n\n")
         response = client.chat(
-            model = model,
+            model=model,
             # https://platform.openai.com/docs/guides/chat-completions/message-roles
-            messages = [
-                {"role" : "user", "content" : prompt}
-            ],
+            messages=[{"role": "user", "content": prompt}],
         )
-        text_output.write(response['message']['content'])
+        text_output.write(response["message"]["content"])
     text_output.close()
