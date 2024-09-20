3e997c7cd438d90d55da4d3fb3a8f1de35efac3c
Conner Smith <smith_ces@outlook.com>
2024-09-19 22:42:49 -0400

    health equity by profound professor prompt and notes

--- toys.py
+++ toys.py
@@ -1,22 +1,26 @@
 import llm
 
-prompt='Describe the style of Emily Dickinson'
-system_prompt="""
-You are a concise, curt, poetry critic. You will be asked about a poet.
-Respond with a brief summary, and an example of the poet's work.
+prompt = """
+Draft a bullet point list of 3 discussion prompts on the topic of Health Equity in the United States.
+Preface this list with a short introductory paragraph.
 """
 
+system_prompt = """
+You are a profound and thoughtful professor.
+An expert on Healthcare in the United States.
+You enjoy use of the socratic method, and leaving things open to interpretation.
+"""
+
+# by duration
+# hermes3 < llama3.1 < phi3.5 < gemma2
 models = [
-    'llama3.1', # good, coherent response; but misquoted a poem
-    'hermes3', # best reponse, adhered to the system_prompt the best, was fastest, and used the most natural language
-    'gemma2', # good response, but seemed robotic and overstructured
-    'phi3.5', # lost coherency through its response
+    "llama3.1",  # the best, but nonetheless, still bland discussion points
+    "hermes3",  # pretty good. paroted "profound" but wasn't that profound
+    "gemma2",  # what a try hard for the "profound professor" thing
+    "phi3.5",  # a bit disjointed
 ]
 
 for model in models:
     llm.write_response(
-        prompt=prompt,
-        system_prompt=system_prompt,
-        model=model,
-        json_name=model
+        prompt=prompt, system_prompt=system_prompt, model=model, json_name=model
     )
